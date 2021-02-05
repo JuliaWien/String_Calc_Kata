0ff2f5996aa88aa4fcce2a2999674d4facd1e06d
unknown <julia.wienkop@gmail.com>
2021-02-05 13:41:03 +0100

    fullfills task e

--- src/sum.js
+++ src/sum.js
@@ -1,13 +1,11 @@
 const { number } = require("yargs");
 
-function stringCalc(numbers) 
-{
+function stringCalc(numbers) {
     var parts, number, result;
- 
+    var errText = '';
 
     if (numbers === '')
         return '';
-
 
     parts = numbers.split(/,|\n/);
 
@@ -20,7 +18,12 @@ function stringCalc(numbers)
 
     for (i = 0; i < parts.length; ++i) {
         if (parts[i] != '') {
+
             number = Number(parts[i]);
+
+            if (number < 0) {
+                throw ('negatives not allowed: ' + errText);
+            }
             result += number;
         }
     }
--- src/sum.test.js
+++ src/sum.test.js
@@ -2,34 +2,42 @@ const { it, expect, } = require("@jest/globals")
 
 const { stringCalc } = require('./sum')
 
-describe('StringCalc', () => {
-    it('should evaluate "" to  " "', () => {
+describe('StringCalc Function Test', () => {
+    it('should evaluate "" to  0', () => {
         const sum = stringCalc(' ')
         expect(sum).toBe(0)
     })
-    it('should evaluate "2" to  "2" ', () => {
 
+    it('should evaluate "2" to  2', () => {
         const sum = stringCalc('2')
         expect(sum).toBe(2)
     })
-    it('should evaluate "1,2" to  "3" ', () => {
+
+    it('should evaluate "1,2" to  3', () => {
         const sum = stringCalc('1,2')
         expect(sum).toBe(3)
     })
-    it('should evaluate "1,2,3" to  "6" ', () => {
+
+    it('should evaluate "1,2,3" to  6 ', () => {
         const sum = stringCalc('1,2,3')
         expect(sum).toBe(6)
     })
-    it('should evaluate "1,2 \n3" to  "6" ', () => {
+
+    it('should evaluate "1,2 \\n3" to  6 ', () => {
         const sum = stringCalc('1,2\n3')
         expect(sum).toBe(6)
     })
-    it('should evaluate "1,2,\n3" to  "6" ', () => {
-        const sum = stringCalc('1,2,\n 3')
+    it('should evaluate "1,2,\\n3" to  6 ', () => {
+        const sum = stringCalc('1,2,\n3')
         expect(sum).toBe(6)
     })
-    it('should evaluate "//;\n1;2;3;4" to  "10" ', () => {
+    it('should evaluate "//;\\n1;2;3;4" to  10 ', () => {
         const sum = stringCalc('//;\n1;2;3;4')
         expect(sum).toBe(10)
+    })
+    it('should give a exception message for negative numbers ', () => {
+        expect(() => {
+            stringCalc('//;\n1;-2000;3;-3')
+          }).toThrow();
     })
 })
